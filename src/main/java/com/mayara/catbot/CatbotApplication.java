64a462ab553939a877dca6858da665c016894b0f
mpaesps <mpaesps133@gmail.com>
2023-09-12 20:49:48 -0300

    first commit

---
+++ src/main/java/com/mayara/catbot/CatbotApplication.java
@@ -0,0 +1,13 @@
+package com.mayara.catbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mayara/catbot/CatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayara.catbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
