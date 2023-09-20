a105776892215afa0a94e9f7feb1c220b7c41337
mpaesps <mpaesps133@gmail.com>
2023-09-19 23:35:21 -0300

    Adicionando função de anexar imagens ao tweet, adicionando dependencia do Tweet4j

--- src/main/java/com/mayara/catbot/services/PostService.java
+++ src/main/java/com/mayara/catbot/services/PostService.java
@@ -1,16 +1,21 @@
 package com.mayara.catbot.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.mayara.catbot.models.FactsResponse;
+import com.mayara.catbot.models.PicturesResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpHeaders;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import com.google.gson.JsonArray;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
@@ -18,7 +23,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Service
 @Slf4j
@@ -29,9 +41,6 @@ public class PostService {
 
     @Autowired
     private ObjectMapper objectMapper;
-
-    @Value("${bearer.token}")
-    private String bearerToken;
 
     @Value("${twitter.api.endpoint}")
     private String twitterApiEndpoint;
@@ -45,14 +54,18 @@ public class PostService {
     @Value("${twitter.oauth}")
     private String OAUTH_STRING;
 
-    @Scheduled(cron = "0 0 12,18 * * ?")
+    @Scheduled(fixedRate = 10000)
     public void orquestraPostNoTwitter() {
 
         try {
             FactsResponse factsResponse = objectMapper.readValue(retornaFatoAleatorio(), FactsResponse.class);
-            fazPostNoTwitter(factsResponse.getData().get(0));
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            List<PicturesResponse> picturesResponse = objectMapper.readValue(retornaFotoAleatoria(), new TypeReference<List<PicturesResponse>>() {
+            });
+
+            fazPostNoTwitter(factsResponse.getData().get(0), picturesResponse.get(0).getUrl());
+        } catch (IOException | TwitterException | InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -74,28 +87,25 @@ public class PostService {
         return realizaHttpRequest(catPicsApiEndpoint);
     }
 
-    private void fazPostNoTwitter(String texto) throws IOException {
+    private void fazPostNoTwitter(String texto, String urlImagem) throws TwitterException, IOException, InterruptedException {
 
-        log.info("Criando objeto JSON");
+        Twitter twitter = Twitter.getInstance();
+        fazDownloadImagem(urlImagem);
 
-        JsonObject jsonBody = new JsonObject();
-        jsonBody.add("text", new JsonPrimitive(texto));
-        StringEntity stringEntity = new StringEntity(new Gson().toJson(jsonBody));
-
-        log.info("Objeto JSON criado com sucesso. Texto a ser postado: " + texto);
+        String mediaId = String.valueOf(twitter.v1().tweets().uploadMedia(new File("image.jpg")).getMediaId());
 
         HttpPost httpPost = new HttpPost(twitterApiEndpoint);
-        httpPost.setEntity(stringEntity);
+        httpPost.setEntity(criaObjetoJsonPost(texto, mediaId));
 
         httpPost.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-        httpPost.setHeader(HttpHeaders.AUTHORIZATION,OAUTH_STRING);
+        httpPost.setHeader(HttpHeaders.AUTHORIZATION, OAUTH_STRING);
 
         log.info("Executando envio de tweet...");
 
         HttpResponse response = httpClient.execute(httpPost);
         int statusCode = response.getStatusLine().getStatusCode();
 
-        if(statusCode == 201) {
+        if (statusCode == 201) {
             log.info("Tweet postado com sucesso.");
         }
 
@@ -103,4 +113,47 @@ public class PostService {
             log.error("Falha ao postar no Twitter: Código de Status " + statusCode);
         }
     }
+
+
+    public void fazDownloadImagem(String url) throws IOException {
+
+        HttpGet httpGet = new HttpGet(url);
+
+        HttpResponse response = httpClient.execute(httpGet);
+        HttpEntity entity = response.getEntity();
+
+        InputStream inputStream = entity.getContent();
+        OutputStream outputStream = new FileOutputStream("image.jpg");
+
+        int bytesRead;
+        byte[] buffer = new byte[1024];
+
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
+
+    }
+
+    private StringEntity criaObjetoJsonPost(String texto, String mediaId) throws UnsupportedEncodingException {
+
+        log.info("Criando objeto JSON");
+
+        JsonObject jsonBody = new JsonObject();
+        jsonBody.add("text", new JsonPrimitive(texto));
+
+        JsonObject mediaObject = new JsonObject();
+        JsonArray mediaIdsArray = new JsonArray();
+        mediaIdsArray.add(new JsonPrimitive(mediaId));
+        mediaObject.add("media_ids", mediaIdsArray);
+
+        jsonBody.add("media", mediaObject);
+
+        log.info("Objeto JSON criado com sucesso: " + jsonBody);
+
+        return new StringEntity(new Gson().toJson(jsonBody));
+
+    }
+
 }
+
+
